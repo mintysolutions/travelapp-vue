d6b3c5cdd89ef1aac365d6b44e2463f73e899a8b
minty <mintysoft.solutions@gmail.com>
2021-08-21 15:59:53 +0000

    feat: add Home, About and Brazil components

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1> Home </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
