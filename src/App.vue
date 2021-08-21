3e64645e57056f3ccfab5c3d2151d29bb7d8be70
minty <mintysoft.solutions@gmail.com>
2021-08-21 16:00:51 +0000

    added links and router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="nav">
+
+    <router-link to="/">Home</router-link> 
+    <router-link to="/about">About</router-link>
+  
+  </div>
+  
+  <div class="container">
+      <router-view></router-view>
+  
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name:'App'
+}
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
 <style>
