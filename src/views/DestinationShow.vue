5d8e906097932e3e7a2a4e28eb8ab81372c45c6b
minty <mintysoft.solutions@gmail.com>
2021-08-21 20:27:25 +0000

    deat:add destination component

---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="destination">
+      <h1>{{destination.name}}</h1>
+      <div class="destination-details">
+          <img :src="`/images/${destination.image}`" :alt="destination.name">
+          <p>{{destination.description}}</p>
+      </div>
+  </section>
+</template>
+
+<script>
+import data from '@/data.json'
+
+export default {
+    name:'DestinationShow',
+    computed:{
+        destinationId(){
+            return parseInt(this.$route.params.id)
+        },
+        destination(){
+            return data.destinations.find(destination => this.destinationId === destination.id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
